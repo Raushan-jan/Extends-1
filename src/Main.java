69e9e723353d17799fc9c83b7b74ca5c4a76f496
Raushan Sydykova <rauchan.87@mail.ru>
2021-12-05 02:11:26 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Rect rect=new Rect(15,15);
+        Rect rect1=new Rect(20);
+        Tringle tringle=new Tringle(20, 23,30);
+        System.out.println(rect.area());
+        System.out.println(tringle.area());
+
+    }
+}
